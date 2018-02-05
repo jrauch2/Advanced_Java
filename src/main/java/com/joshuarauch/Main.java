e1c4d98c164f4c216b90da32198552fae6aeeb76
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-02-04 23:35:08 -0600

    initial commit

--- src/main/java/com/joshuarauch/Main.java
+++ src/main/java/com/joshuarauch/Main.java
@@ -2,6 +2,8 @@ package com.joshuarauch;
 
 public class Main {
 
+    private final static FileRead inFile = new FileRead("sample.txt");
+
     public static void main(String[] args) {
         Encrypt myKey = new Encrypt("Joshua");
         myKey.encrypt('a');
