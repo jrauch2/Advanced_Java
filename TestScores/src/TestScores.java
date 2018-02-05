0bb13c509cc66c1051e76f3e913b017e44a22fa1
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-02-04 20:56:48 -0600

    initial commit

--- TestScores/src/TestScores.java
+++ TestScores/src/TestScores.java
@@ -1,7 +1,17 @@
 import java.util.ArrayList;
 
 public class TestScores {
-    public TestScores(ArrayList testScores) {
+    ArrayList<Decimal> testScores = new ArrayList<>();
 
+    public TestScores(ArrayList testScores) {
+        this.testScores = testScores;
+    }
+
+    public decimal testScoresAverage() {
+        total = 0;
+        for (decimal score: testScores) {
+            total += score;
+        }
+        return total / testScores.size();
     }
 }
