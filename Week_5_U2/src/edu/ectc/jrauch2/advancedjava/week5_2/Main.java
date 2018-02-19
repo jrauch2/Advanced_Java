825920d727228579fce7748b127a3b069144c732
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-02-19 14:42:54 -0600

    finished

--- Week_5_U2/src/edu/ectc/jrauch2/advancedjava/week5_2/Main.java
+++ Week_5_U2/src/edu/ectc/jrauch2/advancedjava/week5_2/Main.java
@@ -1,5 +1,6 @@
 package edu.ectc.jrauch2.advancedjava.week5_2;
 
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -7,11 +8,17 @@ public class Main {
     private static Date theJoshuaTree;
     private static Date rattleAndHum;
 
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         theJoshuaTree = new GregorianCalendar(1987, 3, 9).getTime();
         rattleAndHum = new GregorianCalendar(1988, 10, 10).getTime();
 
         long diff = rattleAndHum.getTime() - theJoshuaTree.getTime();
-        System.out.println("Days between album release: " + (diff / (1144)));
+        System.out.println("Days between album release: " + (diff / (1000 * 60 * 60 * 24)));
+
+        Date future = new Date(new Date().getTime() + diff);
+        DateFormat dateFormatter;
+        dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT);
+        System.out.println("Date of \"Rattle and Hum\": " + dateFormatter.format(future));
+
     }
 }
