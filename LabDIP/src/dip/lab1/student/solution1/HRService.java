251ce718d49632c4ae807f8e2a3a2fac62a42995
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-04-08 19:03:18 -0500

    lab done

---
+++ LabDIP/src/dip/lab1/student/solution1/HRService.java
@@ -0,0 +1,13 @@
+package dip.lab1.student.solution1;
+
+/**
+ *
+ * @author jrauch2
+ */
+public class HRService {
+
+    public double getAnnualCompensationForEmployee(Employee e) {
+
+        return e.getAnnualWages();
+    }
+}
